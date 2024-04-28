61e228a2d66e86923b57d6279daa44ad3b750c26
deligarbur <deliagarciaburgos@gmail.com>
2024-04-28 13:56:44 +0200

    create basic HTML-CSS & JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const nameInput = document.querySelector('.js_search_input');
+const searchBtn = document.querySelector('.js_btn_search');
+const resetBtn = document.querySelector('.js_btn_reset');
+const ulSearchList = document.querySelector('.js_cocktails');
+const error = document.querySelector('.js-error');
+
+let cocktails = [];
+let faveCocktails = [];
