2361be9fe8799fca58b4ed598118a45c1651f537
deligarbur <deliagarciaburgos@gmail.com>
2024-05-03 09:01:42 +0200

    notes

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Variables globales
-const nameInput = document.querySelector( '.js_search_input' );
+let nameInput = document.querySelector( '.js_search_input' );
 const error = document.querySelector( '.js-error' );
 const searchBtn = document.querySelector( '.js_btn_search' );
 const ulSearchList = document.querySelector( '.js_cocktails' );
@@ -13,7 +13,10 @@ let faveCocktails = [];
 
 
 const renderFaves = () => {
+    // pone la ul a vacío para que no se dupliquen los resultados
     ulFaveList.innerHTML = '';
+
+    // dibujará la lista con la clase ‘fave’, y añade el botón X en cada <li>
     for ( const eachCocktail of faveCocktails ) {
         let cocktailImg = '';
         if ( cocktailImg !== null ) {
@@ -29,24 +32,28 @@ const renderFaves = () => {
                 <button class="cocktail_card-delete js_delete_favorite" id="${eachCocktail.idDrink}">X</button>
             </li>`;
     };
-
+    // dibuja el botón eliminarlos todos
     ulFaveList.innerHTML += `<button class="cocktail_card-delete js_delete_all_favorites hidden">Borrar todo</button>`;
 
     favoritesListener();
 }
 
+// Función manejadora para eliminar todos los favoritos
 const handleDeleteAllFaves = () => {
     faveCocktails = [];
     renderFaves();
     renderAllCocktails();
     localStorage.clear();
 }
-
+// Función manejadora para eliminar uno a uno los favoritos (X)
 const handleDeleteFave = ( event ) => {
+    // Cogemos el id del elemento clickado
     const clickedDeleteBtn = event.target.id;
+    // Utilizando findindex, comparo mediante ids y así sé en qué posición está 
     const clickedDeleteCocktailIndex = faveCocktails.findIndex(
         ( item ) => item.idDrink === clickedDeleteBtn
     );
+    // Con splice la quito del listado de favoritos, renderizamos de nuevo, y guardamos en local storage.
     faveCocktails.splice( clickedDeleteCocktailIndex, 1 );
     localStorage.setItem( 'favorites', JSON.stringify( faveCocktails ) );
     renderFaves();
@@ -57,10 +64,17 @@ const favoritesListener = () => {
     const btnDeleteFave = document.querySelectorAll( '.js_delete_favorite' );
     const btnDeleteAllFaves = document.querySelector( '.js_delete_all_favorites' );
 
+    // if ( faveCocktails === 0 ) {
+    //     btnDeleteAllFaves.classList.remove( 'hidden' );
+    // localStorage.clear();
+    // };
+    // otra opción puede ser mediante localstorage. Si es null, que no muestre el botón de borrar todos.
+
     if ( btnDeleteFave !== null ) {
         btnDeleteAllFaves.classList.remove( 'hidden' );
     };
 
+    // Recorremos cada botón X creado en cada cocktail, y escuchamos en la lista completa con el bucle 
     for ( const btn of btnDeleteFave ) {
         btn.addEventListener( 'click', handleDeleteFave );
     };
@@ -76,17 +90,21 @@ const handleAddFavorite = ( event ) => {
     const faveLiClickedIndex = faveCocktails.findIndex(
         ( item ) => item.idDrink === liClickedId
     );
-    // favoriteLiClickedIndex será -1 cuando el id no se encuentre en el array de favoritos
+    // faveLiClickedIndex será -1 cuando el id no se encuentre en el array de favoritos
     if ( faveLiClickedIndex === -1 ) {
+        // si el cocktail no está, lo añade al array con push
         faveCocktails.push( clickedCocktail );
     } else {
+        // si el cocktail ya estaba, lo elimina de la lista con splice
         faveCocktails.splice( faveLiClickedIndex, 1 );
     };
+    // Guarda los favoritos en el localStorage y renderiza todo de nuevo
     localStorage.setItem( 'favorites', JSON.stringify( faveCocktails ) );
     renderAllCocktails( cocktails );
     renderFaves( faveCocktails );
 }
 
+// Recorremos cada elemento creado, cada cocktail, y escuchamos en la lista completa con el bucle 
 const allCocktailsListener = () => {
     const allCocktailsLi = document.querySelectorAll( '.js_li_cocktails' );
     for ( const li of allCocktailsLi ) {
@@ -94,19 +112,22 @@ const allCocktailsListener = () => {
     }
 }
 
-// Función render Listado de Cóctels
+// Función render Listado de Cócteles
 const renderAllCocktails = () => {
-    ulSearchList.innerHTML = '';
+    ulSearchList.innerHTML = ''; // pone la ul a vacío para que no se dupliquen los resultados
     for ( const eachCocktail of cocktails ) {
         let cocktailImg = '';
         if ( cocktailImg !== null ) {
             cocktailImg = eachCocktail.strDrinkThumb;
         } else {
+            // si no encuentra imagen en la API, aparece esta por defecto
             cocktailImg = 'https://www.svgrepo.com/show/206298/cocktail-drink.svg';
         }
+        // busca en el array de favoritos si ya tengo el elemento al que hice click
         const indexFaveCocktail = faveCocktails.findIndex(
             ( item ) => item.idDrink === eachCocktail.idDrink
         );
+        // si no está renderizo con la clase fave (lo añado), y si está renderizo sin la clase (se elimina)
         if ( indexFaveCocktail !== -1 ) {
             ulSearchList.innerHTML += `
                 <li class= "cocktail_card js_li_cocktails fave" id="${eachCocktail.idDrink}">
@@ -120,8 +141,12 @@ const renderAllCocktails = () => {
                 <img class="cocktail_card-img" src="${cocktailImg}">
                 </li>`;
         }
+
         allCocktailsListener();
-    };
+
+
+    }
+
 }
 
 // Funciones Fetch
@@ -147,7 +172,7 @@ const getDataDefault = () => {
         .catch( error => console.log( error ) )
 }
 
-//Funciones manejadoras
+//Función manejadora botón Buscar
 const handleSearch = ( event ) => {
     event.preventDefault();
     const nameCocktail = nameInput.value.toLowerCase();
@@ -158,20 +183,24 @@ const handleSearch = ( event ) => {
         getData( nameCocktail );
     };
 }
-
-const handleReset = () => {
-    nameInput = '';
+// Función manejadora botón reset
+const handleReset = ( event ) => {
+    // limpia campo de búsqueda y listado de resultados
+    event.preventDefault();
     ulSearchList.innerHTML = '';
+    nameInput.value = '';
 }
 
 const init = () => {
+    // Cargo lo almacenado en localstorage
     const cocktailsFaveLocal = localStorage.getItem( 'favorites' );
 
+    // Si localstorage tiene datos almacenados, los renderizo al cargar la página
     if ( cocktailsFaveLocal !== null ) {
         faveCocktails = JSON.parse( cocktailsFaveLocal );
         renderFaves();
     };
-
+    // Al cargar la página mediante un fetch auxiliar, renderizamos en el listado de resultados la lista de margaritas por defecto
     getDataDefault();
 }
 
